36a66200495f7990d7cbca40c478663a5b19274d
Abir Hossain <abir.hossain@sbs.com.au>
2020-02-23 02:15:00 +1100

    master Add MANIFEST file for packaging

--- setup.py
+++ setup.py
@@ -21,5 +21,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
+    include_package_data=True
 )
